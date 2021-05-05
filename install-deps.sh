421320636ed0683e9e36e24019fe1a3619bbc106
十百千 <x1001000@users.noreply.github.com>
2021-05-06 02:47:24 +0800

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -4,7 +4,6 @@ sudo apt install -y python3-pip
 sudo apt install -y portaudio19-dev
 
 sudo pip3 install -U pip
-sudo pip3 uninstall numpy # 1.19.5 causes error
 sudo pip3 install -U numpy==1.19.4 future==0.18.2 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
 sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v45 tensorflow
 sudo pip3 install soundfile pyaudio
